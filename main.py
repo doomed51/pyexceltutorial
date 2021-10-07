34c4ee260c556ed31421530bf94badf5eac7e0b3
Rachit <racshankar@deloitte.ca>
2021-10-07 10:34:01 -0400

    added helpful commands, and iteration through rows

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-from openpyxl import Workbook, workbook
+#load the workbook
+#select sheet by name 
+# 
+#   useful commands
+#
+# sheet.title
+# sheet["A1"].value
+# sheet.cell(row=10, column=6).value
+# sheet["C1"] = "writing ;)"
+# workbook.save(filename="hello_world_append.xlsx")
+#
+# More here: https://realpython.com/openpyxl-excel-spreadsheets-python/
+#
+#   sheet[sheet.max_row]
 
-workbook = Workbook()
-sheet = workbook.active
+from openpyxl import workbook, load_workbook
+from openpyxl.worksheet import worksheet
 
-sheet["A1"] = "hello"
-sheet["A2"] = "world!"
+#load the workbook
+workbook = load_workbook(filename="Sample.xlsm")
 
-workbook.save(filename = "hello_world.xlsx")
+#select sheet by name 
+sheet = workbook["Valuations"]
+
+for row in sheet.iter_rows(values_only=True):
+    print(row)
